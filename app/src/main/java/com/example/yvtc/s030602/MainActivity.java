4243c6a31da51faea7e312ef4e514b4584951c1a
dannielccw <dannielccw@gmail.com>
2017-03-06 13:32:06 +0800

    next version

--- app/src/main/java/com/example/yvtc/s030602/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s030602/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity
         implements View.OnClickListener
 {
     Button btn, btn2;
+    ToggleButton tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         btn = (Button) findViewById(R.id.button);
         btn2 = (Button) findViewById(R.id.button2);
+        tb = (ToggleButton) findViewById(R.id.toggleButton);
         btn.setOnClickListener(this);
         btn2.setOnClickListener(this);
     }
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity
                     break;
                 case R.id.button2:
                     Log.d("BTN", "Test2");
+                    if (tb.isChecked())
+                    {
+                        Log.d("TB","有開");
+                    }
+                    else
+                    {
+                        Log.d("TB", "關閉中");
+                    }
                     break;
 
             }
