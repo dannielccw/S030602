6d23dfa9dacc20cda5fbc9d00c721ab5d5989d01
dannielccw <dannielccw@gmail.com>
2017-03-06 11:46:57 +0800

    next version

--- app/src/main/java/com/example/yvtc/s030602/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s030602/MainActivity.java
@@ -6,27 +6,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener
+{
     Button btn, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        btn =(Button) findViewById(R.id.button);
-        btn2 =(Button) findViewById(R.id.button2);
+        btn = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("BTN", "Test1");
-            }
-        });
+    @Override
+    public void onClick(View v) {
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("BTN", "Test2");
+            switch (v.getId()) {
+                case R.id.button:
+                    Log.d("BTN", "Test1");
+                    break;
+                case R.id.button2:
+                    Log.d("BTN", "Test2");
+                    break;
+
             }
-        });
     }
 }
